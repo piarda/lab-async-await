b25f42569f5621600e5f0179f1728ffd92423b2d
piarda <matt.rindini@gmail.com>
2025-07-23 12:02:09 -0400

    fetched date from API, displayed data, used async/await

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your code here!
+document.addEventListener("DOMContentLoaded", () => {
+  const postList = document.getElementById("post-list");
+
+  // This function displays the posts
+  function displayPosts(posts) {
+    posts.forEach(post => {
+      const li = document.createElement("li");
+      const h1 = document.createElement("h1");
+      const p = document.createElement("p");
+
+      h1.textContent = post.title;
+      p.textContent = post.body;
+      li.appendChild(h1);
+      li.appendChild(p);
+      postList.appendChild(li);
+    });
+  }
+  // This async function fetches from the API and displays the posts
+  async function fetchPosts() {
+    try {
+      const response = await
+      fetch("https://jsonplaceholder.typicode.com/posts");
+      const posts = await response.json();
+      displayPosts(posts);
+    } catch (error) {
+      console.error("Fetch error:", error);
+      postList.innerHTML = "<li>Error loading posts</li>";
+    }
+  }
+  // This calls the async fetch function
+  fetchPosts();
+});
